ff08e17ade65c0c3b66959015ece6eea30c44a7a
Leonardo Situmorang <leonardo.situmorang@gmail.com>
2024-01-09 21:53:10 +0700

    [cli] checkpoint

---
+++ cli/main.go
@@ -0,0 +1,65 @@
+// https://gist.github.com/rivo/2893c6740a6c651f685b9766d1898084
+// https://github.com/cosmtrek/air
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/rivo/tview"
+)
+
+var (
+	app      *tview.Application
+	pages    *tview.Pages
+	debugger *tview.List
+)
+
+func main() {
+	app = tview.NewApplication()
+
+	setup(app)
+
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setup(app *tview.Application) {
+	placeholder := tview.NewList().ShowSecondaryText(false)
+
+	databases := tview.NewList().ShowSecondaryText(false)
+	databases.
+		SetBorder(true).
+		SetTitle("Databases")
+
+	// Populate dummy data
+	for i := 1; i <= 100; i++ {
+		databases.AddItem(fmt.Sprintf("asgard-%d", i), "", 0, func(idx int) func() {
+			return func() {
+				// fmt.Println(fmt.Sprintf("asgard-%d selected", idx))
+			}
+		}(i))
+	}
+
+	navbar := NewNavbar("Menu")
+
+	body := tview.NewFlex().
+		AddItem(navbar, 0, 10, true).
+		AddItem(databases, 0, 40, false).
+		AddItem(placeholder, 0, 50, false)
+
+	debugger = tview.NewList()
+	debugger.
+		SetBorder(true).
+		SetTitle("Debugger")
+
+	// Create the layout
+	layout := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(body, 0, 3, true).
+		AddItem(debugger, 0, 1, false)
+
+	pages := tview.NewPages().AddPage("shell", layout, true, true)
+	app.SetRoot(pages, true)
+}
---
+++ cli/navbar.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rivo/tview"
+)
+
+type (
+	NavbarItem struct {
+		label       string
+		description string
+		shortcut    rune
+		onClick     func()
+	}
+)
+
+var items = []NavbarItem{
+	{
+		label:       "VPN",
+		description: "",
+		shortcut:    0,
+		onClick: func() {
+			fmt.Println("VPN navbar item clicked")
+		},
+	},
+	{
+		label:       "Database",
+		description: "",
+		shortcut:    0,
+		onClick: func() {
+			fmt.Println("Database navbar item clicked")
+		},
+	},
+}
+
+func NewNavbar(title string) *tview.List {
+	list := tview.NewList()
+
+	for _, item := range items {
+		list.AddItem(item.label, item.description, item.shortcut, item.onClick)
+	}
+
+	list.
+		ShowSecondaryText(false).
+		SetBorder(true).
+		SetTitle(title)
+
+	return list
+}
